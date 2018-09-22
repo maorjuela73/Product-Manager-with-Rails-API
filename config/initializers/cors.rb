67228aee57a97313c5484f61326c8cb507e2192b
Miguel Orjuela <ma.orjuela73@gmail.com>
2018-09-22 10:43:57 -0500

    Se instala el JWT

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,14 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+  	# Todos los orígenes
+    origins '*'
+
+    # Todos los recursos
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
